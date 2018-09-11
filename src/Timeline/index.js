1551adc980c662f2a2dfd6cf42809441c2124212
Botmasher <languagejosh@hotmail.com>
2018-09-11 11:12:29 -0700

    Create temporary data store and generate timeline from passed-in content

--- src/Timeline/index.js
+++ src/Timeline/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import { TimelineNode } from '../TimelineNode';
 
-export const Timeline = () => (
+export const Timeline = ({ entries }) => (
   <div className="timeline">
-    
-    <div className="timeline-base"></div>
-
     <div className="timeline-nodes">
-
-      <TimelineNode
-        direction="left"
-        title="Written Mongol"
-        content="the standardized, unified written language in the Mongolian script"
-      />
-
-      <TimelineNode
-        direction="right"
-        title="Middle Mongol"
-        content="the language of the imperial period captured in the Secret History"
-      />
-
-      <TimelineNode
-        direction="left"
-        title="Proto-Mongolic"
-        content="the language reconstructed from the many modern Mongolic varieties"
-      />
-
+      {entries.map((entry, i) => (
+        <TimelineNode
+          key={entry.id}
+          direction={i % 2 ? "left" : "right"}
+          title={entry.title}
+          year={`${entry.year}`}
+          content={entry.content}
+        />
+      ))}
     </div>
-
+    <div className="timeline-base">&nbsp;</div>
   </div>
 );
--- src/TimelineNode/index.js
+++ src/TimelineNode/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export const TimelineNode = ({ direction, title, content }) => (
+export const TimelineNode = ({ direction, title, year, content }) => (
 	<div className={`timeline-node-${direction}`}>
-		<div className="timeline-marker"></div>
+		{direction == 'right' && (<div className="timeline-marker"></div>)}
 		<div className="timeline-content">
 			<h2>{title}</h2>
+			<p><em>{year}</em></p>
 			<p>{content}</p>
 		</div>
+		{direction == 'left' && (<div className="timeline-marker"></div>)}
 	</div>
 );
